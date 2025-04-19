96d7ba4249639e45337fc1a90d1c9e704e4fa90e
DISHA SHARMA <sharmadisha25july@gmail.com>
2025-04-19 22:06:13 +0530

    Stack using 2 Queues
    creating LIFO Structure from FIFO structure

---
+++ Queue/Stack_2Queue.java
@@ -0,0 +1,80 @@
+// Using two FIFO structure {Queue} to LIFO {Stack}
+import java.util.*;
+public class Stack_2Queue {
+    static Queue<Integer> q1=new LinkedList<>();
+    static Queue<Integer> q2=new LinkedList<>();
+    public static boolean isEmpty(){
+        return q1.isEmpty() & q2.isEmpty();
+    }
+    public static void push(int data){
+        if(!q1.isEmpty()){
+            q1.add(data);
+        }else{
+            q2.add(data);
+        }
+    }
+    public static int  pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty ");
+            return -1;
+        }
+       int top=-1;
+       if(!q1.isEmpty()){
+        while(!q1.isEmpty()){
+            top=q1.remove();
+            if(q1.isEmpty()){
+                break;
+            }
+            q2.add(top);//Adding element from queue 1 from queue 2
+        }
+       }
+       else{
+        while(!q2.isEmpty()){
+            top=q2.remove();
+            if(q2.isEmpty()){
+                break;
+            }
+            q1.add(top);
+        }
+       }
+       return top ;
+    }
+    // peek and pop has same approach
+    public static int peek(){  
+      if(isEmpty()){
+        System.out.println("stack is empty ");
+        return -1;
+      }
+      int top=-1;
+      if(!q1.isEmpty()){
+        while(!q1.isEmpty()){
+            top =q1.remove();
+            q2.add(top);
+        }
+      }
+      else{
+        if(!q1.isEmpty()){
+            while(!q1.isEmpty()){
+                top =q1.remove();
+                q2.add(top);
+            }
+        }
+    }
+    return top;
+ } 
+ public static void main(String args[]){
+    Stack<Integer> s =new Stack<>();
+    s.push(1);
+    s.push(2);
+    while(!s.isEmpty()){
+        System.out.println(s.peek()+" ");
+        s.pop();
+    }
+
+ }
+}
+
+
+
+    
+
